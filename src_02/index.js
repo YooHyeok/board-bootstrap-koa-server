7b2c6094c97e12d44ddec036e38aa3f82d2f9a29
webdevyoo <yjk7454@naver.com>
2024-07-28 00:20:18 +0900

    기본 예제코드 디렉토리 변경

---
+++ src_02/index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  console.log('레드벨벳 멤버 소개를 하겠습니다.')
+  ctx.set('Redvelvet-Leader', 'irene');
+  await next();
+  console.log('멤버 소개 완료.')
+});
+
+app.use(async (ctx, next) => {
+  console.log('1. 아이린')
+  await next();
+});
+app.use(async (ctx, next) => {
+  console.log('2. 슬기')
+  await next();
+});
+app.use(async (ctx, next) => {
+  console.log('3. 웬디')
+  await next();
+});
+app.use(async (ctx, next) => {
+  console.log('4. 초이')
+  await next();
+});
+
+app.use(async (ctx,) => {
+  console.log('5. 예리')
+  ctx.body = '레드벨벳 멤버 소개';
+});
+
+app.listen(3000);
