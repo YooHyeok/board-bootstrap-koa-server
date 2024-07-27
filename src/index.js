0c736e506af86ec78ea2a4b690e5f85356fbd338
webdevyoo <yjk7454@naver.com>
2024-07-28 00:22:11 +0900

    feat: `src/index.js` - koa-router 적용

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-/* app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.body = '메인페이지 입니다.'
+})
+
+// url : /other/동방신기
+router.get('/other/:group', (ctx) => {
+  const {group} = ctx.params; // group: 동방신기
+  ctx.body = `${group}의 페이지 입니다.` // 동방신기의 페이지입니다.
+})
+
+// url : /member?name=아이린&id=3 | query : { name: '아이린', id: '3' }
+router.get('/member', (ctx) => {
+  const {name} = ctx.query; // 아이린
+  const {id} = ctx.query; // 3
+  console.log(ctx.query)
+  ctx.body = `레드벨벳 ${id} 번째 멤버 ${name}의 페이지 입니다.`
+})
+
+/**
+ * middleware 배열에 router의 경로:routes() / 콜백함수:allowedMethods() 저장
  */
 
-app.use(async (ctx, next) => {
-  console.log('레드벨벳 멤버 소개를 하겠습니다.')
-  ctx.set('Redvelvet-Leader', 'irene');
-  await next();
-  console.log('멤버 소개 완료.')
-});
-
-app.use(async (ctx, next) => {
-  console.log('1. 아이린')
-  await next();
-});
-app.use(async (ctx, next) => {
-  console.log('2. 슬기')
-  await next();
-});
-app.use(async (ctx, next) => {
-  console.log('3. 웬디')
-  await next();
-});
-app.use(async (ctx, next) => {
-  console.log('4. 초이')
-  await next();
-});
-
-app.use(async (ctx, next) => {
-  console.log('5. 예리')
-  ctx.body = '레드벨벳 멤버 소개';
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
